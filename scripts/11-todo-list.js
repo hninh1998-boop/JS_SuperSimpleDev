3c340374a1e15355af37ec039f1693f4547e8b76
hninh1998 <hninh1998@gmail.com>
2026-04-20 16:37:15 +0700

    update Course

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,25 +1,25 @@
 const todoList = [];
 const todoList2 = [];
 
-function addTodoList1() {
-  const inputElement = document.querySelector('.js-input1');
-  
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+
   todoList.push(inputElement.value);
   console.log(todoList);
 
   inputElement.value = '';
 }
 
-function addTodoList2() {
-  const inputElement = document.querySelector('.js-input2');
-  todoList2.push(inputElement.value);
+function addTodo2() {
+  const inputElement2 = document.querySelector('.js-input2');
+  todoList2.push(inputElement2.value);
+  inputElement2.value = '';
 
-  const pElement = document.querySelector('.js-p');
+  let todoListHTML2 = '';
 
-  for (i = 0; i <= todoList2.length; i++) {
-    if (i = todoList2.length) {
-      pElement.innerHTML += `${todoList2[i]}<br>`;
-    }
+  for (let i = 0; i < todoList2.length; i++) {
+    todoListHTML2 += `<p>${todoList2[i]}</p>`;
   }
-
+  const divElement = document.querySelector('.js-div');
+  divElement.innerHTML = todoListHTML2;
 }
