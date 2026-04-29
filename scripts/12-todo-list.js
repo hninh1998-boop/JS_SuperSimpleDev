e4fef5af95b6f46248022e61228a2423f4b51155
hninh1998 <hninh1998@gmail.com>
2026-04-29 13:47:13 +0700

    update Course

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,75 @@
+const todoList = [];
+const todoList2 = [];
+
+const todoListName3 = [];
+const todoListDate3 = [];
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+
+  todoList.push(inputElement.value);
+  console.log(todoList);
+
+  inputElement.value = '';
+}
+
+function addTodo2() {
+  const inputElement2 = document.querySelector('.js-input2');
+  todoList2.push(inputElement2.value);
+  inputElement2.value = '';
+
+  let todoListHTML2 = '';
+
+  for (let i = 0; i < todoList2.length; i++) {
+    todoListHTML2 += `<p>${todoList2[i]}</p>`;
+  }
+  const divElement = document.querySelector('.js-div');
+  divElement.innerHTML = todoListHTML2;
+}
+//////////////////////////////////////////////////////////////////////////////////
+const todoList3 = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList3();
+
+function renderTodoList3() {
+  let todoListHTML3 = '';
+
+  todoList3.forEach(function(todoObject3, index) {
+    const { name, dueDate } = todoObject3;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList3.splice(${index},1);
+        renderTodoList3
+      " class = "del-todo-btn">Delete</button>
+    `;
+    todoListHTML3 += html;
+  });
+
+  const divElement = document.querySelector('.js-div3');
+  divElement.innerHTML = todoListHTML3;
+}
+
+function addTodo3() {
+  const inputElement3 = document.querySelector('.js-input-name3')
+  const name = inputElement3.value;
+
+  const dataInputElement3 = document.querySelector('.js-input-date3');
+  const dueDate = dataInputElement3.value;
+
+  todoList3.push({
+    name,
+    dueDate
+  });
+
+  inputElement3.value = '';
+
+  renderTodoList3();
+}
